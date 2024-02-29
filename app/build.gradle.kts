94a01dbc5c98b88b87c86d85dab7535cf4659617
negiseijin <satoru6221@gmail.com>
2024-02-29 10:14:20 +0900

    update ci20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.sample.android"
         minSdk = 33
         targetSdk = 33
-        versionCode = 2000
-        versionName = "0.0.2"
+        versionCode = 123001
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
